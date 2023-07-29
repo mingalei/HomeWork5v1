8e408773aee30f0e0b9d1b065d4670877fa0c681
mingalei <mingalei33@gmail.com>
2023-07-29 16:14:13 +0300

    Initial commit

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,24 +1,26 @@
-import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class DragAndDropTest
-{
+public class DragAndDropTest {
     @BeforeAll
-    static void beforeAll()
-    {
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy="eager";
+    static void beforeAll() {
+        BaseTest.beforeAll();
     }
 
     @Test
-    void changeElementsTest()
-    {
+    void changeElementsTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo($("#column-b"));
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
+    }
+
+    @AfterAll
+    static void afterAll() {
+        BaseTest.afterAll();
     }
 }
--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -1,27 +1,28 @@
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class GitHubTest
-{
+public class GitHubTest {
     @BeforeAll
-    static void beforeAll()
-    {
-
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy="eager";
-
+    static void beforeAll() {
+        BaseTest.beforeAll();
     }
+
     @Test
-    void searchWikiTextTest()
-    {
+    void searchWikiTextTest() {
         open("https://github.com/");
-        $(Selectors.byTagAndText("button", "Solutions" )).click();
+        $(Selectors.byTagAndText("button", "Solutions")).click();
         $(".HeaderMenu--logged-out").$(byText("Enterprise")).click();
         $(".application-main").shouldHave(text("Build like the best"));
+    }
+
+    @AfterAll
+    static void afterAll() {
+        BaseTest.afterAll();
     }
 }
